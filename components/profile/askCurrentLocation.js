7e76844d7ef405e6fc90c5e8cc9bcc19a3a8bfca
Shashi Sharma <shashi.sharma1@publicissapient.com>
2023-02-02 22:12:55 +0530

    maps implemented

--- components/profile/askCurrentLocation.js
+++ components/profile/askCurrentLocation.js
@@ -1,76 +1,72 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, Marker, Autocomplete, LoadScript } from '@react-google-maps/api';
 
 function AskCurrentLocation(){
   const [center, setCenter] = React.useState({lat: 20.593683, lng: 78.962883});
+  const [map, setMap] = React.useState(null)
+
   const containerStyle = {
-    width: '500px',
+    width: '100%',
     height: '500px'
   };
+  const mapkey = "AIzaSyBRB_v3kerIoYi7bS6d9kfuNqL-VTRrr1E"
 
-  const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: "AIzaSyBRB_v3kerIoYi7bS6d9kfuNqL-VTRrr1E"
-    })
-
-
-
-    const [map, setMap] = React.useState(null)
 
     const onLoad = React.useCallback(function callback(map) {
       navigator?.geolocation.getCurrentPosition(
         ({coords}) => {
-          let newCenter;
-          console.log(coords)
-          console.log(coords.latitude, coords.longitude)
-          newCenter = {lat: coords.latitude, lng: coords.longitude}
+          const newCenter = {lat: coords.latitude, lng: coords.longitude}
           setCenter(newCenter)
-          console.log(newCenter)
           
           const bounds = new window.google.maps.LatLngBounds(newCenter);
           map.fitBounds(bounds);
           setMap(map)
         }
-      );
-      // google.maps.event.addListener(map, "bounds_changed", () => {
-      //   console.log(map.getBounds());
-      //   this.setState({ bounds: map.getBounds() });
-      // });
-
-
+      )
     }, [])
+  
+  React.useEffect(() => {
 
-    // onMapLoad = map => {
-    //   navigator?.geolocation.getCurrentPosition(
-    //     ({ coords: { latitude: lat, longitude: lng } }) => {
-    //       const pos = { lat, lng };
-    //       this.setState({ currentLocation: pos });
-    //     }
-    //   );
-    //   google.maps.event.addListener(map, "bounds_changed", () => {
-    //     console.log(map.getBounds());
-    //     this.setState({ bounds: map.getBounds() });
-    //   });
-    // };
+  })
 
     const onUnmount = React.useCallback(function callback(map) {
       setMap(null)
     }, [])
 
-    return isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          <Marker position={center} draggable={true} onPositionChanged={(event) => {
-            console.log(event)
-            console.log("dragend")
-          }} />
-        </GoogleMap>
-    ) : <></>
+  return (
+    <>
+      <LoadScript googleMapsApiKey={mapkey} libraries={["places"]}>
+        <div className='container my-10'>
+          <Autocomplete
+            onPlaceChanged={(event) => {
+              console.log(event)
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Enter your address"
+              className='input input-border input-primary w-full'
+            />
+        </Autocomplete>
+        </div>
+        <div>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={15}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            <Marker position={center} draggable={true} onDragEnd={(evt) => {
+              const newpos = { lat: evt.latLng.lat(), lng: evt.latLng.lng() }
+              setCenter(newpos)
+            }} />
+          </GoogleMap>
+        </div>
+
+      </LoadScript>
+      </>
+    )
 }
 
 export default React.memo(AskCurrentLocation);
